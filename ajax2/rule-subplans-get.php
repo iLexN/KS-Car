ea82dc9749ebc73331dcd7694275f859acbf38c5
iLexN <ilex.job@gmail.com>
2016-01-15 10:39:48 +0800

    fix ajax json return type eg.int

--- ajax2/rule-subplans-get.php
+++ ajax2/rule-subplans-get.php
@@ -6,4 +6,10 @@ include('../model/sub-plans.php');
 
 $out = SubPlans::findSubPlansByRuleID($_GET['id']);
 header('Content-Type: application/json');
+
+
+foreach ( $out as $k=> &$v) {
+    $v['sortOrder'] = (int) $v['sortOrder'];
+}
+
 echo( json_encode($out) );
