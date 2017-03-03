31a4a4729eae0a7dec55c73b5fa5b954210bdc54
iLexN <ilex.job@gmail.com>
2017-03-03 10:29:29 +0800

    change email , find rule > 1

--- lib/find-rule.php
+++ lib/find-rule.php
@@ -29,7 +29,7 @@ if (!empty($save_rule)) {
     $DetailsInfo = new DetailsInfo;
     $details_ukey = array();
     foreach ($save_rule as $k => $v_ar) {
-        
+
         $dfInfo_ar = $DetailsInfo->getByRule($v_ar['id']);
 
         $calTotalPriceObj = new calTotalPrice($v_ar);
@@ -39,30 +39,31 @@ if (!empty($save_rule)) {
             $save_rule[$k]['premium'] = $match_rule[$k]['premium'] = number_format($v_ar['premium'],2,'.','');
             $count_Third_Party_Only++;
         }
-        $calTotalPriceArray = $calTotalPriceObj->calPrice($quote->allVar['ncd'], $v_ar['price_add']);
+        //$calTotalPriceArray = $calTotalPriceObj->calPrice($quote->allVar['ncd'], $v_ar['price_add']);
+        $calTotalPriceArray = $calTotalPriceObj->calPrice($quote->allVar['ncd'], $v_ar['price_add'],$v_ar['TypeofInsurance']);
 
         $df_ar = array_column($dfInfo_ar, 'value', 'deatils_id');
         $match_rule[$k]['id'] = $v_ar['id'];
         $match_rule[$k]['planName'] = $v_ar['rule_name'];
         $match_rule[$k]['total_price'] = number_format($calTotalPriceArray['total_price'], 0,'.','');
         $save_rule[$k]['TypeofInsurance'] = $match_rule[$k]['TypeofInsurance'] = $v_ar['TypeofInsurance'];
-        
+
         $match_rule[$k]['loading'] = $v_ar['loading'];
         $match_rule[$k]['clientDiscount'] = $v_ar['clientDiscount'];
         $match_rule[$k]['mibValue'] = $calTotalPriceArray['mibValue'];
         $match_rule[$k]['details'] = $df_ar;
-                
+
         $save_rule[$k]['details'] = $dfInfo_ar;
         $save_rule[$k]['subPlans'] = $match_rule[$k]['subPlans'] = SubPlans::findSubPlansByRuleIdWithLang($v_ar['id'], $quote->allVar['lang']);
 
         $details_ukey = array_merge($details_ukey, array_keys($df_ar));
-        
+
         $save_rule[$k]['gross'] = $calTotalPriceArray['gross'];
         $save_rule[$k]['mibValue'] = $calTotalPriceArray['mibValue'];
         $save_rule[$k]['price'] = $calTotalPriceArray['price'];
         $save_rule[$k]['total_price'] = $calTotalPriceArray['total_price'];
     }
-    
+
     $details_ukey = array_column($DetailsInfo->getOrderByID(array_unique($details_ukey)), 'id');
 }
 
@@ -70,7 +71,7 @@ if ( $count_Third_Party_Only >= 2 ){
     //error_log('count third party only');
     require '../lib/PHPMailer/PHPMailerAutoload.php';
     $mail = new \PHPMailer();
-    $mail->setFrom('info@kwiksure.com', 'Kwiksure');
+    $mail->setFrom('motor@kwiksure.com', 'Kwiksure');
     $mail->addAddress('ken@kwiksure.com', 'Ken');
     $mail->addAddress('alex@kwiksure.com', 'Alex');
     $mail->Subject = 'Motor Online Quote Rule hit more than 1 Rule';
